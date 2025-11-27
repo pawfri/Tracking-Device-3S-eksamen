7e960956141a86087f2e469449cb53bb6a272437
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-11-27 12:58:16 +0100

    Add upgrade to selenium tests

--- Backend/TestSeleniumVue/Test1.cs
+++ Backend/TestSeleniumVue/Test1.cs
@@ -1,11 +1,27 @@
-﻿namespace TestSeleniumVue
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace TestSeleniumVue
 {
-    [TestClass]
-    public sealed class Test1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			//Arrange
+			ChromeOptions options = new ChromeOptions();
+			options.AddArgument("--headless=new");
+			IWebDriver driver = new ChromeDriver(options);
+			driver.Navigate().GoToUrl(@"http://127:0.0.1:5500");
+
+			//Act
+			IWebElement button = driver.FindElement(By.Id("testButton1"));
+			var buttonText = button.Text;
+
+			//Assert
+			Assert.AreEqual("Click Me", buttonText);
+
+		}
+	}
 }
