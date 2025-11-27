10abf7f45e1b879cc1f7f5e5194f50a50643b2cc
Mikka Akdag <mia011@edu.zealand.dk>
2025-11-27 13:28:54 +0100

    Add template for barebones Vue application

---
+++ Frontend/js/main.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Welcome to my Vue template',
+        }
+    },
+    methods: {
+        myMethod(){
+
+        },
+    },
+    computed: {
+        myComputed() {
+            return ''
+        },
+        
+    }
+})
