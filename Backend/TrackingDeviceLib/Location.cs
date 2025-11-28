48e20d8d529450ba2d0e2b50825cf9a209efe39b
Valde2609 <valde2609@gmail.com>
2025-11-28 10:15:48 +0100

    Fixed spelling mistake

--- Backend/TrackingDeviceLib/Location.cs
+++ Backend/TrackingDeviceLib/Location.cs
@@ -3,14 +3,14 @@
 public class Location
 {
 	public int Id { get; set; }
-	public string Longtitude { get; set; }
+	public string Longitude { get; set; }
 	public string Latitude { get; set; }
 	public DateTime Date { get; set; }
 
-	public Location(int id, string longtitude, string latitude, DateTime date)
+	public Location(int id, string longitude, string latitude, DateTime date)
 	{
 		Id = id;
-		Longtitude = longtitude;
+		Longitude = longitude;
 		Latitude = latitude;
 		Date = date;
 	}
