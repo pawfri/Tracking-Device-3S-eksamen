ff00bacb673a322f66d7737601bfaf2d4dfb09dd
Mikka Akdag <mia011@edu.zealand.dk>
2025-11-28 11:01:10 +0100

    Add buttons, styling, organization in stylesheet

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -2,10 +2,21 @@ const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            logninger:[],
+            device: '',
         }
     },
     methods: {
-        myMethod(){
+        getDataFromRaspberry(){
+            axios.get(baseUri)
+            .then(response => {
+                this.logninger = response.data
+                console.log(this.logninger)
+                console.log(response.status);
+            })
+            .catch(error => {
+                console.error("Kunne ikke hente data og sætte dem ind i listen", error);
+            });
 
         },
     },
