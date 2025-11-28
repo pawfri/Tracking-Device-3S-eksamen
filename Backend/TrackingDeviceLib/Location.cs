f41d8ba3a5cef8ef7b0807d58954c94dc4754b42
Valde2609 <valde2609@gmail.com>
2025-11-28 09:42:39 +0100

    Add Location class / Repo / Interface, and controller class for tracking device

---
+++ Backend/TrackingDeviceLib/Location.cs
@@ -0,0 +1,17 @@
+﻿namespace TrackingDeviceLib;
+
+public class Location
+{
+	public int Id { get; set; }
+	public string Longtitude { get; set; }
+	public string Latitude { get; set; }
+	public DateTime Date { get; set; }
+
+	public Location(int id, string longtitude, string latitude, DateTime date)
+	{
+		Id = id;
+		Longtitude = longtitude;
+		Latitude = latitude;
+		Date = date;
+	}
+}
---
+++ Backend/TrackingDeviceLib/Services/Repositories/TrackingDeviceRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingDeviceLib.Services.Repositories;
+
+public class TrackingDeviceRepo
+{
+}
