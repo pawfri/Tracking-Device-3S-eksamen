5562e22329e4114c66a99eb363852b4ec8d7953a
Valde2609 <valde2609@gmail.com>
2025-11-28 09:48:41 +0100

    Created Get, GetAll, Add methods to Location Repository

--- Backend/TrackingDeviceLib/Services/Repositories/TrackingDeviceRepo.cs
+++ Backend/TrackingDeviceLib/Services/Repositories/TrackingDeviceRepo.cs
@@ -8,4 +8,23 @@ namespace TrackingDeviceLib.Services.Repositories;
 
 public class TrackingDeviceRepo
 {
+	private readonly List<Location> _locations = new();
+	private int _nextId = 1;
+
+	public List<Location> GetAll()
+	{
+		return _locations;
+	}
+
+	public Location? GetById(int id)
+	{
+		return _locations.FirstOrDefault(l => l.Id == id);
+	}
+
+	public Location Add(Location location)
+	{
+		location.Id = _nextId++;
+		_locations.Add(location);
+		return location;
+	}
 }
