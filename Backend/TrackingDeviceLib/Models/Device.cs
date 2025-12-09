ef39406a3e97f152eb7af8b2b2020860f5dfff24
Tobias Bech Knudsen <tok005@edu.zealand.dk>
2025-12-09 10:08:58 +0100

    Add 2 new domain classes, Device and User

---
+++ Backend/TrackingDeviceLib/Models/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TrackingDeviceLib.Models;
+
+public class Device
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+
+	public Device(int id, string name)
+	{
+		Id = id;
+		Name = name;
+	}
+}
